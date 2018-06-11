446f4a99e5e66f5faf2813e04d2db52e2b870c39
prod1992 <levon1.grigoryan@gmail.com>
2018-06-11 20:46:39 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -40,7 +40,9 @@ import 'html-loader!./index.html';
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', url);
     xhr.onreadystatechange = function () {
-      if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
+      if (xhr.readyState > 3 && xhr.status == 200)
+        success(xhr.responseText);
+        document.getElementById('app').setAttribute('class', '');
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
