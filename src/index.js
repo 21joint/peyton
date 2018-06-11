61de3202fdde2be2b7bbd9914e0dffb485a68b89
prod1992 <levon1.grigoryan@gmail.com>
2018-06-11 20:28:16 +0400

    ui

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import 'index.scss';
+import 'html-loader!./index.html';
+
+(function () {
+  var url = 'https://cors-anywhere.herokuapp.com/http://api.publicinput.com:8099/Api/HighlightedComments?key=ser31d0ood0jne967olxy0wwyqng9chf&set=test';
+
+  getAjax(url, generateHTML);
+
+  function generateComments(data) {
+    var _data = JSON.parse(data);
+    var comments = '';
+    for (var j = 0; j < _data.length; j++) {
+      var comment = _data[j].comment;
+      comments += `<li><p class="m-0">${comment}</p></li>`;
+    }
+    return '<ul class="app-topic--comments">' + comments + '</ul>';
+  }
+
+  function generateHTML(data) {
+    document.getElementById('app').innerHTML = `
+                      <div class="container-fluid">
+                        <div class="row">
+                          <div class="col-md-4">
+                            <div class="app-topic">
+                              <div class="app-topic--header">
+                                <h2 class="app-topic--title">The Project Title</h2>
+                                <div><a href="" role="button" class="btn btn-join"><b>Join The Conversation</b></a></div>
+                              </div>
+                              <div class="app-topic--body scrollable-y">
+                                <h3><i>The question goes here...</i></h3>
+                                ${generateComments(data)}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+  }
+
+  function getAjax(url, success) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState > 3 && xhr.status == 200) success(xhr.responseText);
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
+  }
+})();
