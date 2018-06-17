4f5e90d3bd74840af8f877b25f7ce8777ceab253
prod1992 <levon1.grigoryan@gmail.com>
2018-06-17 07:06:57 +0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'index.scss';
 import 'html-loader!./index.html';
+import _ from 'lodash';
 
 (function () {
   var url = 'https://cors-anywhere.herokuapp.com/http://api.publicinput.com:8099/Api/HighlightedComments?key=ser31d0ood0jne967olxy0wwyqng9chf&set=test';
@@ -12,46 +13,47 @@ import 'html-loader!./index.html';
     xhr.onreadystatechange = function () {
       if (xhr.readyState > 3 && xhr.status == 200)
         success(xhr.responseText);
-        removeClass(document.getElementById('app'), 'loading');
+      removeClass(document.getElementById('app'), 'loading');
     };
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
     return xhr;
   }
-  function generateComments(data) {
-    var _data = JSON.parse(data);
-    var comments = '';
-    for (var j = 0; j < _data.length; j++) {
-      var comment = _data[j].comment;
-      comments += `<li><p class="m-0">${comment}</p></li>`;
-    }
-    return '<ul class="app-topic--comments">' + comments + '</ul>';
-  }
+
   function generateHTML(data) {
-    let commentsHtml = generateComments(data);
-    document.getElementById('app').innerHTML = `
-                      <div class="container-fluid">
-                        <div class="row">
-                          <div class="col-md-4">
+    var _data = _.sortBy(_.groupBy(JSON.parse(data), 'project.id'), 'length').reverse();
+    var _res = '';
+    _.each(_data, function (project) {
+      var _commentsHtml = '';
+      var articleTitle = '';
+      var projectName = '';
+      _.each(project, function (dt) {
+        console.log(dt);
+        projectName = dt.project.name;
+        _commentsHtml += '<li><p class="m-0">' + dt.comment + '</p></li>';
+        articleTitle = dt.article.name;
+      });
+      _res += `<div class="col-md-4">
                             <div class="app-topic">
                               <div class="app-topic--header">
-                                <h2 class="app-topic--title">The Project Title</h2>
-                                <div><a href="" role="button" class="btn btn-join"><b>Join The Conversation</b></a></div>
+                                <h2 class="app-topic--title">${projectName}</h2>
+                                <div><a href="#" role="button" class="btn btn-join"><b>Join The Conversation</b></a></div>
                               </div>
                               <div class="app-topic--body scrollable-y">
-                                <h3><i>The question goes here...</i></h3>
-                                ${commentsHtml}
+                                <h3><i>${articleTitle}</i></h3>
+                                <ul>${_commentsHtml}</ul>
                               </div>
                             </div>
-                          </div>
-                        </div>
-                      </div>`;
+                          </div>`;
+    });
+    document.getElementById('app').innerHTML = `<div class="container-fluid"><div class="row">${_res}</div></div>`;
   }
+
   function removeClass(el, className) {
     if (el.classList)
-      el.classList.remove(className)
+      el.classList.remove(className);
     else if (hasClass(el, className)) {
-      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+      var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
       el.className = el.className.replace(reg, ' ')
     }
   }
